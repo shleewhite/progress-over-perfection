9472502caa5b093f831b4779abd5a9ae017d7a96
Lee White <leewhite128@gmail.com>
2023-10-13 12:04:21 -0400

    remove unneeded text

--- src/components/SlideTemplates/LinkToSlides.tsx
+++ src/components/SlideTemplates/LinkToSlides.tsx
@@ -6,7 +6,7 @@ export const LinkToSlides: FC = () => (
   <Slide>
     <div className="link-to-slides">
       <p>
-        Link to slides: <a href="https://bit.ly/libjs-pop">bit.ly/libjs-pop</a>
+        Slides: <a href="https://bit.ly/libjs-pop">bit.ly/libjs-pop</a>
       </p>
     </div>
   </Slide>
